b41e8abb31bc62d3f03b5f6eaa2b32017c2c706f
desiree <desireegarcia@mini.hsd1.nm.comcast.net>
2021-10-06 19:09:13 -0600

    added useState and displayed data to page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, {useState, useEffect}  from "react";
 import "./App.css";
+import {BASE_URL, API_KEY} from './index.js';
 import axios from 'axios';
 
-import {BASE_URL, API_KEY} from './index.js';
+
+
 
 
 function App() {
+ 
+  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [explanation, setExplanation] = useState('');
+  const [copyright, setCopyright] = useState('');
+
 
 useEffect(() => {
   axios.get(`${BASE_URL}?api_key=${API_KEY}`)
   .then(res => {
-    console.log(res);
-  }).catch(err=> {
-    console.error(err);
+    const {data } = res;
+    console.log(data);
+    setUrl(data.url)
+    setTitle(data.title);
+    setExplanation(data.explanation);
+    setCopyright(data.copyright);
+    console.log("First Render");
+    })
+   
+  .catch(err=> {
+    console.error(err,"Something happened...");
   })
-}, [])
+}, []);
 
 
   return (
@@ -23,6 +39,11 @@ useEffect(() => {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <h2>{title}</h2>
+      <img src={url} alt={title}></img>
+      <p>{copyright}</p>
+      <p>{explanation}</p>
+      
     </div>
   );
 }
